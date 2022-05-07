a2b2c8bd2dbf58181a14b36e7c9598cba6562f60
Santo Shakil <shakil.mezbah@gmail.com>
2022-05-07 11:53:58 +0600

    Logger

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart' show ProviderScope;
 import 'src/app.dart' show MyApp;
 import 'src/modules/setting/provider/config/app_config_db.dart'
     show openAppConfigDB;
-import 'src/utils/paths/paths.dart' show initDir;
+import 'src/utils/helpers/other/other_helper.dart' show log;
+import 'src/utils/paths/paths.dart' show appDir, initDir;
 
 void main() async {
   await initDir();
+  log.i('App Directory: ${appDir.path}');
   await openAppConfigDB();
   runApp(const ProviderScope(child: MyApp()));
 }
