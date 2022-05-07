b01ea2a86cd2ed65bd48be8d61fccb6c198b37b0
Santo Shakil <shakil.mezbah@gmail.com>
2022-05-07 11:32:10 +0600

    Fixes

--- lib/src/modules/setting/provider/theme/theme_provider.dart
+++ lib/src/modules/setting/provider/theme/theme_provider.dart
@@ -5,10 +5,10 @@ import '../../model/setting_model.dart'
     show AppConfigExtension, GetAppConfigCollection;
 import '../config/app_config_db.dart' show appConfig, appConfigDB;
 
-final themeConfigStream =
+final appConfigStream =
     StreamProvider((ref) => appConfigDB.appConfigs.watchObjectLazy(0));
 
 final themeProvider = Provider((ref) {
-  ref.watch(themeConfigStream);
+  ref.watch(appConfigStream);
   return appConfig.theme;
 });
