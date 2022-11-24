0cf7741b2d478402a86a91ef48e5354fe7b0f131
Santo Shakil <shakil.mezbah@gmail.com>
2022-11-24 22:32:41 +0600

    Fixes

--- lib/src/db/isar.dart
+++ lib/src/db/isar.dart
@@ -2,14 +2,18 @@ import 'package:flutter/foundation.dart' show kReleaseMode;
 import 'package:isar/isar.dart' show Isar;
 
 import '../modules/setting/model/setting.model.dart' show AppSettingSchema;
-import 'paths.dart' show AppDir, initDir;
+import 'paths.dart' show AppDir, appDir, initDir;
 
 const _schema = [AppSettingSchema];
 late final Isar db;
 
 Future<void> openDB() async {
   await initDir();
-  db = await Isar.open(_schema, inspector: !kReleaseMode, directory: db.path);
+  db = await Isar.open(
+    _schema,
+    inspector: !kReleaseMode,
+    directory: appDir.db.path,
+  );
 }
 
 void openDBSync(AppDir dir) => db =
