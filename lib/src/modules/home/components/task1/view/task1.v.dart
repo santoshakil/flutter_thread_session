ce486735e58c31f1350802c287a688dfa8075e8f
Santo Shakil <shakil.mezbah@gmail.com>
2023-02-09 23:57:55 +0600

    test

--- lib/src/modules/home/components/task1/view/task1.v.dart
+++ lib/src/modules/home/components/task1/view/task1.v.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Task1View extends ConsumerWidget {
   const Task1View({Key? key}) : super(key: key);
@@ -13,8 +19,22 @@ class Task1View extends ConsumerWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             FilledButton.tonal(
-              onPressed: () {},
               child: const Text('Task 1'),
+              onPressed: () async {
+                final file = await rootBundle.loadString('data/json/csc.json');
+                final data = jsonDecode(file) as List;
+                final query = data
+                    .where((e) => (e['states'] as List).length > 10)
+                    .toList();
+                print(query.length);
+                final formatted = jsonEncode(query);
+                final dir = await getApplicationDocumentsDirectory();
+                final newFile = File(join(dir.path, 'new.json'))
+                  ..writeAsStringSync(formatted);
+                print(newFile.path);
+                await newFile.delete();
+                print(newFile.existsSync());
+              },
             ),
           ],
         ),
