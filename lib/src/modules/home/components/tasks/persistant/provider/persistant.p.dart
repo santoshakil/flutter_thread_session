b15930b441712c9d6469d2fc020b48629d1c3406
Santo Shakil <shakil.mezbah@gmail.com>
2023-02-10 23:21:35 +0600

    Persistant Thread

--- lib/src/modules/home/components/tasks/persistant/provider/persistant.p.dart
+++ lib/src/modules/home/components/tasks/persistant/provider/persistant.p.dart
@@ -1,1 +1,23 @@
+import 'dart:async';
+import 'dart:isolate';
 
+Future<void> persistantThread() async {
+  final receivePort = ReceivePort();
+
+  await Isolate.spawn(
+    (sendPort) {
+      Timer.periodic(
+        const Duration(seconds: 1),
+        (timer) {
+          print('In Secondery Thread: ${DateTime.now()}');
+          sendPort.send('${DateTime.now()}');
+        },
+      );
+    },
+    receivePort.sendPort,
+  );
+
+  await for (final message in receivePort) {
+    print('In Main Thread: $message');
+  }
+}
--- lib/src/modules/home/components/tasks/persistant/view/persistant.v.dart
+++ lib/src/modules/home/components/tasks/persistant/view/persistant.v.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../button/task.button.v.dart';
 import '../../../task/model/task.m.dart';
+import '../provider/persistant.p.dart';
 
 class PersistantView extends ConsumerWidget {
   const PersistantView({super.key});
@@ -16,7 +17,7 @@ class PersistantView extends ConsumerWidget {
         TaskButton(
           task: Tasks.persistence,
           onTap: () async {
-            await Future.delayed(const Duration(seconds: 100));
+            await persistantThread();
           },
         ),
         const SizedBox(height: 10),
