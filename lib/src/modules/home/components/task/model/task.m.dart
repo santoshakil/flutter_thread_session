1e11c3f605a79550bae54f4207852bae0d73a0a2
Santo Shakil <shakil.mezbah@gmail.com>
2023-02-10 17:57:57 +0600

    Topics

--- lib/src/modules/home/components/task/model/task.m.dart
+++ lib/src/modules/home/components/task/model/task.m.dart
@@ -45,3 +45,17 @@ enum Tasks {
   final String description;
   final Widget view;
 }
+
+// The topics covered in the session of Basic to Advance threading in Flutter/Dart are as follows:
+
+// Compute: Compute is used to perform short-lived heavy tasks in a separate isolate. It uses the "compute" function to perform these tasks.
+
+// Isolate: Isolate.spawn is used to perform long-lived heavy tasks in a separate isolate. This allows for the creation of a new thread that is separate from the main thread and can perform tasks without affecting the main thread.
+
+// Synchronization: Synchronization is used to ensure thread-safe data access. This is important when multiple threads need to access and modify the same data, as it ensures that data integrity is maintained.
+
+// Persistence Thread: The persistence thread is used to perform heavy tasks in a separate isolate, but it always listens to the main isolate for some function calls. This is useful for performing tasks that require a long duration to complete, while still allowing the main thread to perform other tasks in the meantime.
+
+// Two-way Data Passing: Two-way data passing is used to pass data from the main isolate to a separate isolate, and vice versa. This allows for communication between the main thread and the separate thread, making it possible to coordinate tasks and share data between the two.
+
+// Other: The "Other" topic covers other heavy task examples. This can include any additional examples of tasks that may need to be performed in a separate isolate to improve performance or ensure data integrity.
