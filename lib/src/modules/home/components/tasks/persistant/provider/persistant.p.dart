3ce1a4ffa4ce8c01f08f8b17ff2b1b64a12fa3ad
Santo Shakil <shakil.mezbah@gmail.com>
2023-02-10 23:49:20 +0600

    Persistant Thread

--- lib/src/modules/home/components/tasks/persistant/provider/persistant.p.dart
+++ lib/src/modules/home/components/tasks/persistant/provider/persistant.p.dart
@@ -4,20 +4,40 @@ import 'dart:isolate';
 Future<void> persistantThread() async {
   final receivePort = ReceivePort();
 
-  await Isolate.spawn(
+  final isolate = await Isolate.spawn(
     (sendPort) {
       Timer.periodic(
         const Duration(seconds: 1),
         (timer) {
-          print('In Secondery Thread: ${DateTime.now()}');
-          sendPort.send('${DateTime.now()}');
+          print('In Secondery Thread: ${timer.tick}');
+          sendPort.send(timer.tick);
+          if (timer.tick == 5) {
+            timer.cancel();
+          }
         },
       );
     },
     receivePort.sendPort,
+    // onExit: receivePort.sendPort,
   );
 
-  await for (final message in receivePort) {
+  isolate.addOnExitListener(
+    receivePort.sendPort,
+    response: 'Isolate exited',
+  );
+
+  // await for (final message in receivePort) {
+  //   print('In Main Thread: $message');
+  //   if (message == 10) {
+  //     receivePort.close();
+  //   }
+  // }
+  receivePort.listen((message) {
     print('In Main Thread: $message');
-  }
+    // if (message == 7) {
+    //   receivePort.close();
+    // }
+  });
+
+  print('Done');
 }
